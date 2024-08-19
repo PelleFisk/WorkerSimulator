49ded8956245b22e367e5a28393656a068ddb80b
PelleFisk <adrian.o.forsberg@icloud.com>
2024-08-19 21:25:37 +0200

    updated the WorkerModel to have new properties

--- WorkerSimulator/Models/WorkerModel.cs
+++ WorkerSimulator/Models/WorkerModel.cs
@@ -7,4 +7,6 @@ public class WorkerModel
 	public MaterialType Material { get; private set; }
 	public int Fuel { get; private set; }
 	public int Speed { get; private set; }
+	public float WorkTime { get; private set; }
+	public int Level { get; private set; }
 }
